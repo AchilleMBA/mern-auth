05e7b0119377fe049b308daf8e51e87bbac4cb10
Achille Maboa <achille.maboa@icloud.com>
2023-11-28 14:35:21 +0100

    creation api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+dotenv.config();
 
 const app = express();
 
@@ -16,3 +19,5 @@ mongoose
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
+
+app.use('/api/user', userRoutes);
