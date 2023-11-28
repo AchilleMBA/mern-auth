d01b628e64d975ec19dc8ae4225a3149ac062c95
Achille Maboa <achille.maboa@icloud.com>
2023-11-28 11:03:18 +0100

    installation react tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function App() {
+  return (<h1 className="text-red-500 font-bold underline">
+  Hello world!
+</h1>
+  )
+}
+
+export default App
