c31b1e95fa2666d9c00e1e08f34170a39d85ccf9
Achille Maboa <achille.maboa@icloud.com>
2023-11-28 12:23:35 +0100

    connection mongo db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
